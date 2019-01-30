a1a296602f651888b4e088318bd738286d040b24
Chris Han <chha@microsoft.com>
2019-01-30 19:14:49 +0800

    update aml_agent

--- donkeycar/templates/aml_agent.py
+++ donkeycar/templates/aml_agent.py
@@ -1,15 +1,91 @@
-#!/usr/bin/env python3
+# -------------------------------------------------------------------------
+# Access Azure File Share with SAS
+# --------------------------------------------------------------------------
+from azure.common import (
+    AzureConflictHttpError,
+    AzureMissingResourceHttpError,
+)
+from azure.storage.common import (
+    AccessPolicy,
+    ResourceTypes,
+    AccountPermissions,
+)
+from azure.storage.file import (
+    FileService,
+    SharePermissions,
+    FilePermissions,
+)
 
-import azureml.core
-from azureml.core import Workspace, Datastore
+import sys
+from os import listdir
+from os.path import isfile, join
 
-# Check core SDK version number
-print("SDK version:", azureml.core.VERSION)
 
-ws = Workspace.from_config()
-print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
+class AmlAgent():
+    def __init__(self):
+        self.file_service = FileService(account_name='mtcautonstoragegpdxmrdy', sas_token='?sv=2018-03-28&ss=bfqt&srt=sco&sp=rwdlacup&se=2029-01-30T15:34:49Z&st=2019-01-30T07:34:49Z&sip=0.0.0.0-255.255.255.255&spr=https,http&sig=bizcj%2FZqjH4aB7bRsvYPUYDyqK90dy4DB%2B6JTx7D1Hs%3D')
+        self.share_name ="azureml-filestore-3c6c3e55-de73-4e41-a9ed-bb06bb9f26cc"
+    '''
+        # Basic
+        # List from root
+        root_file_dir = list(self.file_service.list_directories_and_files(share_name))
+        for res in root_file_dir:
+            print(res.name)  # dir1, dir2, rootfile
 
-def_file_store = Datastore(ws, "workspacefilestore")
-print("Default datastore's name: {}".format(def_file_store.name))
+        # Num results
+        root_file_dir = list(self.file_service.list_directories_and_files(share_name, num_results=2))
+        for res in root_file_dir:
+            print(res.name)  # dir1, dir2
 
-def_file_store.upload ("./tub", target_path = "./tub", overwrite = True, show_progress = True)
+        # List from directory
+        dir1 = list(self.file_service.list_directories_and_files(share_name, 'test data'))
+        for res in dir1:
+            print(res.name)  # file1, file2
+    '''
+    def upload_data(self):
+        print("uploading data...")
+        data_dir_name='./tub'
+        self.copy_files(data_dir_name)                  
+
+
+
+    def copy_files(self, data_dir_name):
+            if self.file_service.exists(data_dir_name)==False:
+                dest_directory_name = self.file_service.create_directory(self.share_name, data_dir_name)
+            else:
+                dest_directory_name =data_dir_name
+
+            files_in_source_dir = [f for f in listdir(data_dir_name) if isfile(join(data_dir_name, f))]
+
+            for file_name in files_in_source_dir:
+                full_source_file_name = join(data_dir_name, '/', file_name)
+                print(file_name)  # file1, file2
+                #source = self.file_service.make_file_url(self.share_name, dest_directory_name, file_name)
+                #copy = self.file_service.copy_file(self.share_name, None, 'file1copy', source)
+                self.file_service.create_file_from_path(
+                    self.share_name,              # share name
+                    dest_directory_name,                   # directory path - root path if none
+                    file_name,               # destination file name
+                    full_source_file_name,      # full source path with file name
+                    progress_callback=self.generate_progress_callback(file_name)) #report progress
+
+
+    def generate_progress_callback(file_name):
+        def progress_callback(current, total):
+            print('({}, {}, {})'.format(file_name, current, total))
+        return progress_callback
+
+
+    def download_model(self):
+        print("downloading model...")
+
+
+    if __name__ == '__main__':
+        args = sys.argv
+
+        if args[0]=='download':
+            download_model()
+        else:
+            upload_data()
+
+     
