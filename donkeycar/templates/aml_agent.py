38776620ac5adc2a18fc55ddb573a09d9fbf6795
Chris Han <chha@microsoft.com>
2019-01-31 19:05:16 +0800

    storage account changed after switch to http from https

--- donkeycar/templates/aml_agent.py
+++ donkeycar/templates/aml_agent.py
@@ -26,7 +26,7 @@ class AmlAgent():
     file_service=None
 
     def __init__(self):
-        self.file_service = FileService(account_name='mtcautonstoragegpdxmrdy', 
+        self.file_service = FileService(account_name='chrisamlstoragemjeiyhfu', 
             sas_token='?sv=2018-03-28&ss=bfqt&srt=sco&sp=rwdlacup&se=2029-01-31T18:58:16Z&st=2019-01-31T10:58:16Z&spr=https,http&sig=FMkEUPqTAT7%2BmXZXFjeBaQ1Ydoik8Kic1ZYcGtHYc3w%3D',
             protocol='http')
         self.share_name ='azureml-filestore-3c6c3e55-de73-4e41-a9ed-bb06bb9f26cc'
